6f712e029465d91501561578711334e3fd6b2f76
Ekaterina <93206172+kleoby@users.noreply.github.com>
2022-01-01 15:06:56 +0300

    feat:update index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let myArray = Array.from(String(n), Number);
+
+    if (Math.sign(n) === -1) {
+        myArray.shift(0, 1);
+        const reversed = myArray.reverse();
+        let result = Number(reversed.join(''));
+
+        return result;
+    } else {
+        const reversed = myArray.reverse();
+        let result = Number(reversed.join(''));
+        return result;
+    }
 }
